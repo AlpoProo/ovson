2e2da3384f203961d4c02de5697895a51e411604
Alperen Gökdeniz <69711127+AlpoProo@users.noreply.github.com>
2026-01-19 01:46:22 +0300

    Delete OVson directory

--- OVson/OVson/SDK/Player.cpp
+++
@@ -1,9 +0,0 @@
-#include "Player.h"
-
-CPlayer::CPlayer(jobject instance) {
-	this->playerInstance = instance;
-}
-
-void CPlayer::Cleanup() {
-	lc->env->DeleteLocalRef(this->playerInstance);
-}
--- OVson/Render/RenderHook.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace RenderHook {
-	bool install();
-	void uninstall();
-	void poll();
-}
